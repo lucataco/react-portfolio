8dd2ff0ecc245977a14a26f0563b2c5ccb7ac4d7
lucataco <lucataco@umd.edu>
2021-05-11 02:20:05 -0400

    Adding google analytics test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 import Profile from "./components/Profile";
+import ReactGa from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize("G-1Q2EM22P6Q");
+    ReactGa.pageview("/");
+  }, []);
+
   return (
     <div className="App">
       <div className="main-info">
         <Profile />
       </div>
-      <div className="footer" style={{ fontSize: 11, color: 'gray' }}>
+      <div className="footer" style={{ fontSize: 11, color: "gray" }}>
         &copy; 2021 @ Luis Catacora
       </div>
     </div>
