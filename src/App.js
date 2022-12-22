ecfb757f93ec6b7441a6bcf6034244fe4f0a9969
lucataco <lucataco@umd.edu>
2022-12-21 22:37:35 -0500

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Profile from "./components/Profile";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="footer" style={{ fontSize: 11, color: "gray" }}>
         &copy; 2022 @ Luis Catacora
       </div>
+      <Analytics />
     </div>
   );
 }
